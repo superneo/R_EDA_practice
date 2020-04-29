67c2b800a9199d402647509b5e165a97624242ca
KeunYoung Park <keunyp@kr.ibm.com>
2020-04-29 15:03:29 +0900

    feat: Complete get_whole_KMA_data

--- src/get_raw_data.R
+++ src/get_raw_data.R
@@ -19,12 +19,28 @@ if("here" %in% rownames(installed.packages()) == FALSE) {
 
 library(here)
 
+KMA_read <- FALSE
+df_KMA <- NULL
+
 get_whole_KMA_data <- function() {
+    if(KMA_read && !is.null(df_KMA)) {
+        print("KMA dataframe is ready already!")
+        return(df_KMA)
+    }
+
     raw_root <- paste0(here(), '/data/KMA_raw_data')
     df1 <- read.csv(paste0(raw_root,
                            "/SEOUL_2000_2009_OBS_ASOS_DD_20200427153022.csv"))
-    str(df1)
+    df2 <- read.csv(paste0(raw_root,
+                           "/SEOUL_2010_2019_OBS_ASOS_DD_20200427151516.csv"))
+    df3 <- read.csv(paste0(raw_root,
+                           "/SEOUL_2020_2020_OBS_ASOS_DD_20200427152808.csv"))
+    df <- rbind(rbind(df1, df2), df3)
+    str(df)
+    assign("df_KMA", df, envir = .GlobalEnv)
     assign("KMA_read", TRUE, envir = .GlobalEnv)
+
+    return(df_KMA)
 }
 
 get_KMA_data <- function(year_start, year_end) {
